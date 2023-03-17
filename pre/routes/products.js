8ea9e93888881747a8ae5e1c1cd1960d37bf96c1
juanda <juandacorreo@gmail.com>
2023-03-17 13:10:55 +0100

    fix file

--- pre/routes/products.js
+++ pre/routes/products.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const data = require('../data/prices.json')
+const data = require('./prices.json')
 
 /* GET products listing. */
 router.get('/', function (req, res, next) {

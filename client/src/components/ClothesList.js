8ccf40f1cec871a2c72eafda267421c44c0278df
a22939sv <a22939@svalero.com>
2023-03-20 13:02:36 +0100

    Update ClothesList.js

--- client/src/components/ClothesList.js
+++ client/src/components/ClothesList.js
@@ -13,20 +13,33 @@ const ClothesList = (props) => {
   }, [props.clothes]);
 
   return (
+    //     <div class="card" style="width: 18rem;">
+    //   <img class="card-img-top" src="..." alt="Card image cap">
+    //   <div class="card-body">
+    //     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    //   </div>
+    // </div>
     <div className="text-black row capa">
       {data.length !== 0
         ? data.map((product, index) =>
             product[6] === props.season ? (
               <div
                 key={index}
-                className="image-container d-flex flex-column align-items-center justify-content-center m-3"
+                className="card image-container d-flex flex-column align-items-center justify-content-center m-3"
               >
                 <img
                   src={`/images/${product[0]}.jpg`}
                   title={product[9]}
                   alt={product[9]}
                   width="180px"
+                  className="card-img-top"
                 ></img>
+                <div class="card-body">
+                  <p class="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                </div>
                 <h5>{product[9]}</h5>
               </div>
             ) : (

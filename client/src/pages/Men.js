927fbc756ad75db4708e5de7f69e94fa8ffa9b02
a22939sv <a22939@svalero.com>
2023-03-20 18:26:38 +0100

    Update Men.js

--- client/src/pages/Men.js
+++ client/src/pages/Men.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Men = () => {
-  return <></>;
+  return (
+    <div className="container-fluid m-0 p-0">
+      <header>
+        <NavBar />
+      </header>
+      <main className="container-fluid text-white">
+        <div className="row"></div>
+      </main>
+    </div>
+  );
 };
 
 export default Men;

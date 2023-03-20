b1a5608ef378d34a76e7ee0232a74a7902ca6102
a22939sv <a22939@svalero.com>
2023-03-20 12:24:40 +0100

    Update ClothesList.js

--- client/src/components/ClothesList.js
+++ client/src/components/ClothesList.js
@@ -4,7 +4,7 @@ const ClothesList = (props) => {
   const [data, setData] = useState([]);
 
   const formatData = (clothes) => {
-    setData(clothes.slice(0, 16));
+    setData(clothes.slice(0, 35));
   };
 
   useEffect(() => {

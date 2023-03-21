16a42202951c4928591afceffafcd24b9747016f
a22939sv <a22939@svalero.com>
2023-03-21 10:17:58 +0100

    Update StartBox.js

--- client/src/components/StartBox.js
+++ client/src/components/StartBox.js
@@ -3,10 +3,10 @@ import React from "react";
 const StartBox = () => {
   return (
     <form className="d-flex">
-      <button className="inputLogIn" title="Log In">
+      <button className="inputLogIn fw-bold" title="Log In">
         Log In
       </button>
-      <button className="inputSignIn" title="Sign In">
+      <button className="inputSignIn fw-bold" title="Sign In">
         Sign In
       </button>
     </form>
